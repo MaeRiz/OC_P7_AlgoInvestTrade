ee013db9004d5845876f34654051bc353f9c78f1
Mahery Gonin <mahery.gonin@gmail.com>
2021-07-06 10:12:58 +0200

    Add comments and PEP8 check

--- bruteforce.py
+++ bruteforce.py
@@ -3,9 +3,12 @@ import sys
 import csv
 import time
 
-MAX_PRICE = 500
 start_time = time.time()
 
+MAX_PRICE = 500                 # Prix d'achat maximum
+
+
+# Calcul le bénéfice a partir d'une liste d'actions
 def calc_benef(lst):
     calc = []
     for act in lst:
@@ -13,21 +16,25 @@ def calc_benef(lst):
     return (sum(calc))
 
 
+# Calcul la somme des prix d'une liste d'actions
 def somme(lst):
     _sum = []
-    for l in lst:
-        _sum.append(l[1])
+    for item in lst:
+        _sum.append(item[1])
     return (sum(_sum))
 
 
+# =============================================
+# Création et test de toutes les combinaisons
+# =============================================
 def make_sol(lst):
 
     actions_lst = lst
     benef = 0
 
-    for l in range(len(actions_lst)):
+    for i in range(len(actions_lst)):
 
-        combs = combinations(actions_lst, l + 1)
+        combs = combinations(actions_lst, i + 1)
         for comb in combs:
             _sum = somme(comb)
             if _sum <= MAX_PRICE:
@@ -36,29 +43,32 @@ def make_sol(lst):
                 if _benef > benef:
                     benef = _benef
                     best_comb = comb
-    
+
     print('Meilleurs combinaison trouvée: ')
     for comb in best_comb:
         print(comb)
     print('Prix: ', somme(best_comb), '€')
     print('Bénéfice: +', benef, '€ au bout de 2 ans.')
-    print ('Exécution: ', time.time() - start_time, "secondes")
+    print('Exécution: ', time.time() - start_time, "secondes")
 
 
+# ===========================================================================
+#   Traitement du fichier mis en paramètre, convertion en liste exploitable
+# ===========================================================================
 try:
-    with open (sys.argv[1], newline='') as csvfile:
+    with open(sys.argv[1], newline='') as csvfile:
         actions = csv.reader(csvfile, delimiter=',', quotechar='|')
         actions_lst = []
         for rows in actions:
             actions_lst.append(
-                [rows[0],
-                float(rows[1]),
-                float(rows[2].replace('%', ''))]
+                [
+                    rows[0],
+                    float(rows[1]),
+                    float(rows[2].replace('%', ''))
+                ]
             )
 
         make_sol(actions_lst)
 
 except FileNotFoundError:
     print("Le fichier n'existe pas. Veuillez vérifier le nom.")
-
-
--- optimized.py
+++ optimized.py
@@ -3,15 +3,22 @@ import csv
 import time
 from math import ceil
 
-MAX_PRICE=500
 start_time = time.time()
 
+MAX_PRICE = 500                 # Prix d'achat maximum
+
+
+# Calcul la somme des prix d'une liste d'actions
 def somme(lst):
     _sum = []
-    for l in lst:
-        _sum.append(l[1])
+    for item in lst:
+        _sum.append(item[1])
     return (sum(_sum))
 
+
+# ===========================================================================
+#   Fabrication de la matrice et récupération de la meilleur combinaison
+# ===========================================================================
 def sacADos(lst):
 
     price = MAX_PRICE*10
@@ -21,7 +28,10 @@ def sacADos(lst):
     for item in range(1, len(lst) + 1):
         for euro in range(1, price + 1):
             if lst[item-1][1] <= euro:
-                matrice[item][euro] = max(lst[item-1][2] + matrice[item-1][euro-lst[item-1][1]], matrice[item-1][euro])
+                matrice[item][euro] = max(
+                    lst[item-1][2] + matrice[item-1][euro-lst[item-1][1]],
+                    matrice[item-1][euro]
+                )
             else:
                 matrice[item][euro] = matrice[item-1][euro]
 
@@ -29,7 +39,6 @@ def sacADos(lst):
     n = len(lst)
     comb = []
 
-    
     while p >= 0 and n >= 0:
         e = lst[n-1]
         if matrice[n][p] == matrice[n-1][p-e[1]] + e[2]:
@@ -37,18 +46,20 @@ def sacADos(lst):
             p -= e[1]
 
         n -= 1
-    
-    
+
     print('Meilleurs combinaison trouvée: ')
     for c in comb:
         print(c[0])
-    print('Prix: ~', somme(comb)/10 , '€')
+    print('Prix: ~', somme(comb)/10, '€')
     print('Bénéfice: +', matrice[-1][-1], '€ au bout de 2 ans.')
-    print ('Exécution: ', time.time() - start_time, "secondes")
+    print('Exécution: ', time.time() - start_time, "secondes")
 
 
+# ===========================================================================
+#   Traitement du fichier mis en paramètre, convertion en liste exploitable
+# ===========================================================================
 try:
-    with open (sys.argv[1], newline='') as csvfile:
+    with open(sys.argv[1], newline='') as csvfile:
         actions = csv.reader(csvfile, delimiter=',', quotechar='|')
         actions_lst = []
         for rows in actions:
@@ -56,9 +67,12 @@ try:
                 pass
             else:
                 actions_lst.append(
-                    [rows[0],
-                    int(ceil(float(rows[1])*10)),
-                    float(float(rows[1]) * float(rows[2].replace('%', '')) / 100)]
+                    [
+                        rows[0],
+                        int(ceil(float(rows[1])*10)),
+                        float(float(rows[1]) * float(
+                            rows[2].replace('%', '')) / 100),
+                    ]
                 )
 
         sacADos(actions_lst)
